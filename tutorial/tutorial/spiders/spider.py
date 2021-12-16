56848688561755a6e22d5a8aa8677e8c4c2e6a06
Sanjana Gandhi <scgandhi12345@gmail.com>
2021-12-16 22:18:06 +0530

    Commit2

--- tutorial/tutorial/spiders/spider.py
+++ tutorial/tutorial/spiders/spider.py
@@ -1,7 +1,7 @@
 import scrapy
 
-class TutorialSpider(scrapy.Spider):
-    name = 'tutorial'
+class Spider(scrapy.Spider):
+    name = 'scrapy_prj'
 
     def start_requests(self):
         urls = [
@@ -16,13 +16,13 @@ class TutorialSpider(scrapy.Spider):
     def parse(self, response):
         for title in response.css('div.product'):
             price = title.css('div.price-rating-container div.catalog-item-price span span::text').extract_first()
-            heading=title.css('div.product-description a::text').extract_first()
+            title2=title.css('div.product-description a::text').extract_first()
             stock=title.css('div.price-rating-container div.catalog-item-price div div span span::text').extract_first()
             maftr=title.css('div.product-description div a::text').extract_first()
                 
             yield {
                 'price':price,
-                'title':heading,
+                'title':title2,
                 'stock':bool(stock=='In Stock'),
                 'maftr':maftr
             }
